1dd20cf800748f3963fb6f1fac08ff79eaa2b5ff
Svaraikyam <andiotlabs@gmail.com>
2024-10-13 19:27:30 +0530

    Working bazelrc and TensorRT version for tf2.17

--- tensorflow/compiler/tf2tensorrt/stub/nvinfer_stub.cc
+++ tensorflow/compiler/tf2tensorrt/stub/nvinfer_stub.cc
@@ -54,7 +54,7 @@ void LogFatalSymbolNotFound(const char* symbol_name) {
 #error TensorRT version earlier than 7 is not supported.
 #elif NV_TENSORRT_MAJOR == 7
 #include "tensorflow/compiler/tf2tensorrt/stub/NvInfer_7_0.inc"
-#elif NV_TENSORRT_MAJOR == 8
+#elif NV_TENSORRT_MAJOR == 10
 #include "tensorflow/compiler/tf2tensorrt/stub/NvInfer_8_0.inc"
 #else
 #error This version of TensorRT is not supported.
